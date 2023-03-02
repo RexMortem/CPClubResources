ce67d5ca6c09b3885d6d68fb2f85f5f2bf5e4a46
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-03-02 21:23:49 +0000

    Create PythonDivision.py

---
+++ Hackerrank/Python/PythonDivision.py
@@ -0,0 +1,11 @@
+# you can think of integer division as getting the whole number part of a remainder division 
+
+# e.g. 5 / 3 = 1 remainder 2
+# 5 // 3 = 1
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b) 
+    print(a/b)
