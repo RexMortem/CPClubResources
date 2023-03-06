f0d5551ac649460fcbb7e88b606c6d3d036a6754
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-03-06 23:36:36 +0000

    Create Difference.py

---
+++ Hackerrank/Python/Sets/Difference.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+input()
+English = set(map(int, input().split()))
+input()
+French = set(map(int, input().split()))
+
+print(len(English - French))
