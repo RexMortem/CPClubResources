495257b2d4d636e4fad92e805f6e2559781b5ce8
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-03-06 14:38:23 +0000

    Update sWAPcASE.py

--- Hackerrank/Python/Strings/sWAPcASE.py
+++ Hackerrank/Python/Strings/sWAPcASE.py
@@ -4,13 +4,16 @@ def SwapCharacter(c):
     else:
         return c.upper()
 
-def swap_case(s): # unfortunately, no easy way to form strings from map
+def swap_case(s): # procedural way 
     s2 = ""
     
     for x in s:
         s2 += SwapCharacter(x)
         
     return s2
+
+def swap_case_functional(s): # functional way 
+    return "".join(map(SwapCharacter, s))
 
 if __name__ == '__main__':
     s = input()
