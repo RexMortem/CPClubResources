4093f15742e2a0aad538f34b61e1821c187f4a28
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-03-07 00:25:44 +0000

    Update CaptainsRoom.py

--- Hackerrank/Python/Sets/CaptainsRoom.py
+++ Hackerrank/Python/Sets/CaptainsRoom.py
@@ -1,6 +1,6 @@
 # There's a way to do this using Counter module
 # There is a way to do this with list comprehension (hint: count(x) in an array gives you number of occurrences of x in array)
-# There is a way to do this 
+# There is a way to do this mathematically involving the sum of the set, and the sum of the list
 
 K = int(input())
 RoomNumbers = map(int, input().split())
