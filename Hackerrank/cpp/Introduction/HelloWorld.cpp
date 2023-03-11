115f1b3ed0e6573e7b0d15a4d64af44ce0695227
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-03-11 14:40:08 +0000

    Create HelloWorld.cpp

---
+++ Hackerrank/cpp/Introduction/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // for input/output
+
+using namespace std; // std library contains tons of useful stuff; you just need to include the header files! 
+
+int main() {
+    //printf("Hello, World!"); -> this is the C way to do it
+    cout << "Hello, World!";
+    return 0;
+}
