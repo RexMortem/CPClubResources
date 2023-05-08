cc7254b243cfcf572cacea7b9c062524917ffe13
RexMortem <68436000+RexMortem@users.noreply.github.com>
2023-05-08 11:25:12 +0100

    Create ForLoop.cpp

---
+++ Hackerrank/cpp/Introduction/ForLoop.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+string EnglishRepresentation(int a){
+    switch (a){
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        default:
+            return "";
+    }
+}
+
+int main() {
+    // Complete the code.
+    int a,b;
+    
+    cin >> a >> b;
+    
+    for (int i = a; i <= b;i++){
+        if ((i >= 1) and (i <= 9)){
+            cout << EnglishRepresentation(i) << endl;
+        }else if (i > 9){
+            if ((i % 2) == 0){
+                cout << "even" << endl;
+            }else{
+                cout << "odd" << endl;
+            }
+        }
+    }
+    
+    return 0;
+}
